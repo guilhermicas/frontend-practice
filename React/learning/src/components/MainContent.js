f05c6c52ad1459c7dcb5dbe7671da764dc4213e8
guilhermicas <sotrabalhosgui@hotmail.com>
2021-06-03 18:46:04 +0100

    basico de useState, operacao ternaria e chamamento de funcao a partir de elemento

--- React/learning/src/components/MainContent.js
+++ React/learning/src/components/MainContent.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 //import "../css/header.css";
 
 function MainContent() {
+  let [isMorning, setIsMorning] = useState(true);
   // Se eu quiser pôr uma variavel JS no conteudo basta por a variavel dentro de {} dentro do elemento
   let primeiroNome = "Guilherme";
   let ultimoNome = "Silva";
+
+  function handleClick() {
+    setIsMorning(!isMorning);
+  }
 
   // Tudo o que esta escrito dentro de {} sera interpretado como javascript
   // Nao e possivel fazer ifs, só operações ternárias
@@ -16,6 +21,8 @@ function MainContent() {
         {primeiroNome + " " + ultimoNome}
       </p>
       <p>{`${primeiroNome} ${ultimoNome}`}</p>
+      <p>{isMorning ? "É de manha agora" : "Not morning ;()"}</p>
+      <input type="button" onClick={handleClick} value="dar toggle no bool" />
     </main>
   );
 }
